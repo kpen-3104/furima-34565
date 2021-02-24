2a0cd6a68fec22db5fc9b0c544c53723c99b6f5d
kpen-3104 <kpen.3104m@gmail.com>
2021-02-24 20:54:41 +0900

    新規登録画面のエラーハンドリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,9 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_reading])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_reading])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname,:first_name, :last_name, :first_name_reading, :last_name_reading, :birthday]
+    )
+    
   end
 end
