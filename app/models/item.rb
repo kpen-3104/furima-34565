d9f7cfaa87e6ebb0c09bfa0397b00ebb00f0cffa
kpen-3104 <kpen.3104m@gmail.com>
2021-02-26 14:36:26 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attend :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :delivery_fee
+  belongs_to :shipping_area
+  belongs_to :day_to_ship
 end
