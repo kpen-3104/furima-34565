7aacd423737b0a592a8c7907d0a632ccdf8bb983
kpen-3104 <kpen.3104m@gmail.com>
2021-02-26 20:36:49 +0900

    itemモデルのバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,20 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :shipping_area
   belongs_to :day_to_ship
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_description
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :item_condition_id
+      validates :delivery_fee_id
+      validates :shipping_area_id
+      validates :day_to_ship_id
+    end
+    validates  :item_price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} 
+  end
 end
+
+    
