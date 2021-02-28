747a8ba015f54407a4271e75022a9d331c4957bb
kpen-3104 <kpen.3104m@gmail.com>
2021-02-28 20:40:46 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new( item_params )
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :item_description, :category_id, :item_condition_id, :delivery_fee_id, :shipping_area_id, :day_to_ship_id, :item_price  ).merge( user_id: current_user.id )
+    params.require(:item).permit(:image, :item_name, :item_description, :category_id, :item_condition_id, :delivery_fee_id,
+                                 :shipping_area_id, :day_to_ship_id, :item_price).merge(user_id: current_user.id)
   end
-
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :item do
-    item_name         {'テスト'}
-    item_description  {'データが保存されるか'}
+    item_name         { 'テスト' }
+    item_description  { 'データが保存されるか' }
     category_id       { 2 }
     item_condition_id { 2 }
     delivery_fee_id   { 2 }
-    shipping_area_id  { 2 } 
+    shipping_area_id  { 2 }
     day_to_ship_id    { 2 }
-    item_price        { 300 } 
+    item_price        { 300 }
     association :user
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
-   end
+    end
   end
 end
