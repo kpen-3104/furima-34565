6273d0ab7e599bdabd737fdbcb481ad4d1d90d06
kpen-3104 <kpen.3104m@gmail.com>
2021-03-01 20:03:42 +0900

    商品情報編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,6 +45,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
